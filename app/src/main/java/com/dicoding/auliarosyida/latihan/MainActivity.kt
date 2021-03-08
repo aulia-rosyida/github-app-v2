6fb81bab22950f8b97a8519af488b7a524b42449
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-03-08 10:55:41 +0700

    form layout

---
+++ app/src/main/java/com/dicoding/auliarosyida/latihan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.auliarosyida.latihan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
