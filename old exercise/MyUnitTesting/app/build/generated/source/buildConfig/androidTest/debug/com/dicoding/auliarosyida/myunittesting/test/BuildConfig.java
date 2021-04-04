0cc91628f349caf0a854a4e54b45f1d3d0b6b45f
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-04 09:10:28 +0700

    merapikan folder

---
+++ old exercise/MyUnitTesting/app/build/generated/source/buildConfig/androidTest/debug/com/dicoding/auliarosyida/myunittesting/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dicoding.auliarosyida.myunittesting.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dicoding.auliarosyida.myunittesting.test";
+  public static final String BUILD_TYPE = "debug";
+}
