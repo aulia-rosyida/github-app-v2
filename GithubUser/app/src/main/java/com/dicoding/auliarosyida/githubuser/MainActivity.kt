e69f1bdebd679e04fe39f6aa426c956a89a01c2f
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-05 11:09:54 +0700

    changes listView to recyclerView

--- GithubUser/app/src/main/java/com/dicoding/auliarosyida/githubuser/MainActivity.kt
+++ GithubUser/app/src/main/java/com/dicoding/auliarosyida/githubuser/MainActivity.kt
@@ -7,72 +7,71 @@ import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.auliarosyida.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var title: String = "Github User List"
-
-    private lateinit var adapter: UserAdapter
-    private lateinit var dataUsername: Array<String>
-    private lateinit var dataName: Array<String>
-    private lateinit var dataLocation: Array<String>
-    private lateinit var dataRepository: Array<String>
-    private lateinit var dataCompany: Array<String>
-    private lateinit var dataFollowers: Array<String>
-    private lateinit var dataFollowing: Array<String>
-    private lateinit var dataPhoto: TypedArray
+    private lateinit var binding: ActivityMainBinding
     private var users = arrayListOf<User>()
+
+//    private lateinit var adapter: UserAdapter
+//    private lateinit var dataUsername: Array<String>
+//    private lateinit var dataName: Array<String>
+//    private lateinit var dataLocation: Array<String>
+//    private lateinit var dataRepository: Array<String>
+//    private lateinit var dataCompany: Array<String>
+//    private lateinit var dataFollowers: Array<String>
+//    private lateinit var dataFollowing: Array<String>
+//    private lateinit var dataPhoto: TypedArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setActionBarTitle(title)
 
-        val listView: ListView = findViewById(R.id.lv_list)
-        adapter = UserAdapter(this)
-        listView.adapter = adapter
-
-        //untuk inisialisasi array
-        prepare()
-        //untuk memasukkan data
-        addItem()
-
-        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            Toast.makeText(this@MainActivity, users[position].name, Toast.LENGTH_SHORT).show()
-            val moveWithObjectIntent = Intent(this@MainActivity, DetailUser::class.java)
-            moveWithObjectIntent.putExtra(DetailUser.EXTRA_USER, users[position])
-            startActivity(moveWithObjectIntent)
-        }
-    }
-
-    //menyiapkan data
-    private fun prepare() {
-        dataUsername = resources.getStringArray(R.array.username)
-        dataName = resources.getStringArray(R.array.name)
-        dataLocation = resources.getStringArray(R.array.location)
-        dataRepository = resources.getStringArray(R.array.repository)
-        dataCompany = resources.getStringArray(R.array.company)
-        dataFollowers = resources.getStringArray(R.array.followers)
-        dataFollowing = resources.getStringArray(R.array.following)
-        dataPhoto = resources.obtainTypedArray(R.array.avatar)
-    }
-    private fun addItem() {
-        for (position in dataName.indices) {
-            val user = User(
-                dataUsername[position],
-                dataName[position],
-                dataLocation[position],
-                dataRepository[position],
-                dataCompany[position],
-                dataFollowers[position],
-                dataFollowing[position],
-                dataPhoto.getResourceId(position, -1),
-            )
-            users.add(user)
-        }
-        adapter.users = users
+        binding.rvList.setHasFixedSize(true)
+        users.addAll(getListUsers())
+        showRecyclerList()
     }
 
     private fun setActionBarTitle(title: String) {
         supportActionBar?.title = title
+    }
+
+    private fun getListUsers(): ArrayList<User> {
+
+        val dataUsername = resources.getStringArray(R.array.username)
+        val dataName = resources.getStringArray(R.array.name)
+        val dataLocation = resources.getStringArray(R.array.location)
+        val dataRepository = resources.getStringArray(R.array.repository)
+        val dataCompany = resources.getStringArray(R.array.company)
+        val dataFollowers = resources.getStringArray(R.array.followers)
+        val dataFollowing = resources.getStringArray(R.array.following)
+        val dataPhoto = resources.obtainTypedArray(R.array.avatar)
+
+        val listUser = ArrayList<User>()
+        for (position in dataUsername.indices) {
+            val user = User(
+                    dataUsername[position],
+                    dataName[position],
+                    dataLocation[position],
+                    dataRepository[position],
+                    dataCompany[position],
+                    dataFollowers[position],
+                    dataFollowing[position],
+                    dataPhoto.getResourceId(position, -1)
+            )
+            listUser.add(user)
+        }
+        return listUser
+    }
+
+    private fun showRecyclerList() {
+        binding.rvList.layoutManager = LinearLayoutManager(this)
+        val listUserAdapter = UserAdapter(users)
+        binding.rvList.adapter = listUserAdapter
     }
 }
